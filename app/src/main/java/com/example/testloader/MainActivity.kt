4b66a729ee7e8c6f6134a6a78ef7d5d7e69d5ca6
David dts. Tellez Solano <dts@inforserveis.local>
2022-02-11 20:59:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testloader/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.testloader
+
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var ivLoader: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getViews()
+    }
+
+    private fun getViews(){
+        ivLoader = findViewById(R.id.ivLoader)
+        animateView(ivLoader!!)
+    }
+
+    private fun animateView(view: ImageView) {
+        val animated = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_anim_settings)
+        animated?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) {
+                //Loop animation
+                view.post { animated.start() }
+            }
+
+        })
+        view.setImageDrawable(animated)
+        //Put the color you want
+        //view.setColorFilter(Color.BLACK)
+        //Start animation
+        animated?.start()
+    }
+}
